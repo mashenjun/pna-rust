63899eebcd89db37fc4cfdac0d0009db33a2113c
Shenjun Ma <mashenjun0902@gmail.com>
2020-12-03 14:06:28 +0800

    cli: finish project1 part1 to part6
    Signed-off-by: Shenjun Ma <mashenjun0902@gmail.com>

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,55 @@
+extern crate clap;
+use clap::{App, Arg, SubCommand};
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    // access env variables
+    let version: &str = env!("CARGO_PKG_VERSION");
+    let author: &str = env!("CARGO_PKG_AUTHORS");
+
+    let matches = App::new("kvs")
+        .version(version) // todo read form toml
+        .author(author)
+        .about("cli for in memory kv store")
+        .subcommand(
+            SubCommand::with_name("get").about("get data").arg(
+                Arg::with_name("key")
+                    .required(true)
+                    .help("get data by the given key"),
+            ),
+        )
+        .subcommand(
+            SubCommand::with_name("set")
+                .about("set data")
+                .arg(
+                    Arg::with_name("key")
+                        .required(true)
+                        .help("data to set into"),
+                )
+                .arg(Arg::with_name("value").required(true).help("data to set")),
+        )
+        .subcommand(
+            SubCommand::with_name("rm")
+                .about("remove data")
+                .arg(Arg::with_name("key").required(true).help("remove data")),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        ("get", Some(_m)) => {
+            eprintln!("unimplemented");
+            process::exit(1);
+        }
+        ("set", Some(_m)) => {
+            eprintln!("unimplemented");
+            process::exit(1);
+        }
+        ("rm", Some(_m)) => {
+            eprintln!("unimplemented");
+            process::exit(1);
+        }
+        _ => {
+            process::exit(1);
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,31 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+//! # KvStore Crate
+#![deny(missing_docs)]
+use std::collections::HashMap;
+
+/// KvStore store data in memory
+pub struct KvStore {
+    db: HashMap<String, String>,
+}
+
+#[allow(clippy::new_without_default)]
+impl KvStore {
+    /// new init a instance of KvStore by using HashMap
+    pub fn new() -> Self {
+        Self { db: HashMap::new() }
+    }
+
+    /// set use internal HashMap to set data
+    pub fn set(&mut self, key: String, value: String) {
+        self.db.insert(key, value);
+    }
+
+    /// get use internal HashMap to get data
+    pub fn get(&mut self, key: String) -> Option<String> {
+        self.db.get(&key).cloned()
+    }
+
+    /// remove call internal HashMap remove api to remove data
+    pub fn remove(&mut self, key: String) {
+        self.db.remove(&key);
     }
 }
